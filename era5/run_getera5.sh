4cd0d248b97b268346ba26c32436c427b506405e
James Ruppert <jruppert.jr@gmail.com>
2023-05-31 11:44:02 -0500

    updated model domain

--- era5/run_getera5.sh
+++ era5/run_getera5.sh
@@ -1,23 +1,29 @@
 #!/bin/bash -l
 
-DATE1=20070818
-DATE2=20070819
+
+DATE1=20070817
+DATE2=20070821
+Nort=46
+West=-118
+Sout=20
+East=-80
+
+
 YY1=`echo $DATE1 | cut -c1-4`
 MM1=`echo $DATE1 | cut -c5-6`
 DD1=`echo $DATE1 | cut -c7-8`
 YY2=`echo $DATE2 | cut -c1-4`
 MM2=`echo $DATE2 | cut -c5-6`
 DD2=`echo $DATE2 | cut -c7-8`
-Nort=45
-West=-118
-Sout=21
-East=-78
+
 
 sed -e "s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;s/Nort/${Nort}/g;s/West/${West}/g;s/Sout/${Sout}/g;s/East/${East}/g;" GetERA5-sl.py > GetERA5-${DATE1}-${DATE2}-sl.py
 
-#python GetERA5-${DATE1}-${DATE2}-sl.py
+
+python GetERA5-${DATE1}-${DATE2}-sl.py
+
 
 sed -e "s/DATE1/${DATE1}/g;s/DATE2/${DATE2}/g;s/Nort/${Nort}/g;s/West/${West}/g;s/Sout/${Sout}/g;s/East/${East}/g;" GetERA5-pl.py > GetERA5-${DATE1}-${DATE2}-pl.py
 
-#python GetERA5-${DATE1}-${DATE2}-pl.py
 
+python GetERA5-${DATE1}-${DATE2}-pl.py
